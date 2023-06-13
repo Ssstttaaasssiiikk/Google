06d92ae61e587d34b5c94ebc17ddbc4e0ca3ff86
Ssstttaaasssiiikk <ssstttaaasssiiikk@gmail.com>
2023-06-13 18:24:23 +0700

    создал вотрую страницу, но пока без переходов

---
+++ src/SecondPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Second_page(){
+    return(
+        <body>
+            <span>Нахуй ты сюда пришел?</span>
+        </body>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as SecondPage from './SecondPage'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Link to='/'>Home</Link>
+      <Link to = '/secondPage'>Second_Page</Link>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
