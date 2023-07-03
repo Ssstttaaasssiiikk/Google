dad31a8564071009c482a6d32c1f5c884519c67f
ssstttaaasssiiikk <ssstttaaasssiiikk@gmail.com>
2023-07-03 16:02:00 +0400

    переделал ввод и вывод из input

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -4,9 +4,6 @@ import { Navigation } from '../components/Nav';
 export function Counter(){
     const [counter, setCounter] = useState<number>(0)
     const [userText, setUserText] = useState<string>('')
-    const userTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUserText(event.target.value);
-      };
     return(
         <>
             <Navigation name_page = 'google' way = '/'/>
@@ -15,13 +12,12 @@ export function Counter(){
                 <span>{counter}</span>
                 <button onClick={() => setCounter(counter + 1)}>+</button>
             </div>
-            <div className='search_bar'>
-                <input type="text" value={userText} onChange={userTextChange} />
-            </div>
             <div>
                 <span>{userText}</span>
             </div>
-            
+            <div className='search_bar'>
+                <input type="text" value={userText} onChange={event => setUserText(event.target.value)} />
+            </div> 
         </>
     );
 }
